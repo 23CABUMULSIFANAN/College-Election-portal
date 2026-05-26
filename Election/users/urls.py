aa2a9f8c0c4ab17a0e85ac4cad0880cd1182f303
umulsifananasarali@gmail.com <umulsifananasarali@gmail.com>
2026-05-26 20:09:52 +0530

    fix typo in views import

--- Election/users/urls.py
+++ Election/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import Stuent_List,generate_otp,verify_otp,admin_login
+from .views import student_list,generate_otp,verify_otp,admin_login
 urlpatterns=[
-    path("student/",Stuent_List),
+    path("student/",student_list),
     path("generate_otp/",generate_otp),
     path("verify_otp/",verify_otp),
     path('admin-login/', admin_login),
