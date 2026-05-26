72c3c92be342d8fe6b9c6734ff60d05e69699949
umulsifananasarali@gmail.com <umulsifananasarali@gmail.com>
2026-05-26 19:10:33 +0530

    fix email backend crash

--- Election/Election/settings.py
+++ Election/Election/settings.py
@@ -154,13 +154,14 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 CORS_ALLOW_CREDENTIALS = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
 EMAIL_HOST_USER = 'umulsifananasarali@gmail.com'
-EMAIL_HOST_PASSWORD = 'dummy_password'
+EMAIL_HOST_PASSWORD = 'oqsjdsdjptxletsw'
 
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
