76707ede9691d4307742c6f5cab542b99f76c5a0
umulsifananasarali@gmail.com <umulsifananasarali@gmail.com>
2026-05-26 20:21:36 +0530

    fix deployment config and cleanup

--- Election/Election/settings.py
+++ Election/Election/settings.py
@@ -160,7 +160,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
 EMAIL_HOST_USER = 'umulsifananasarali@gmail.com'
-EMAIL_HOST_PASSWORD = 'oqsjdsdjptxletsw'
+EMAIL_HOST_PASSWORD = 'hfcysjpczaucpzpt'
 
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
