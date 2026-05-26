d803ef9fb1373d55a5906f6237f5e7eb6496c0c3
umulsifananasarali@gmail.com <umulsifananasarali@gmail.com>
2026-05-26 13:12:07 +0530

    fix: update API base URL to college-election-portal

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://college-election-api.onrender.com/api",
+    baseURL:  "https://college-election-portal.onrender.com",
 });
 
 export default api;
