8171c721134fcc1d1090a9fd06a944eba093e903
umulsifananasarali@gmail.com <umulsifananasarali@gmail.com>
2026-05-26 15:49:34 +0530

    force redeploy frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:  "https://college-election-portal.onrender.com",
+    baseURL: "https://college-election-portal.onrender.com/api",
 });
 
 export default api;
