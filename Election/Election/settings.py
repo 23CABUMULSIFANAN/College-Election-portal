29eca015ba2385bd5edd95c35418f82e9c8a14ed
umulsifananasarali@gmail.com <umulsifananasarali@gmail.com>
2026-05-26 20:33:09 +0530

    fix deployment config and cleanup

--- Election/Election/settings.py
+++ Election/Election/settings.py
@@ -159,8 +159,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'umulsifananasarali@gmail.com'
-EMAIL_HOST_PASSWORD = 'hfcysjpczaucpzpt'
+# EMAIL_HOST_USER = 'umulsifananasarali@gmail.com'
+# EMAIL_HOST_PASSWORD = 'hfcysjpczaucpzpt'
+
+import os
+
+EMAIL_HOST_PASSWORD = os.environ.get("hfcysjpczaucpzpt")
+EMAIL_HOST_USER = os.environ.get("umulsifananasarali@gmail.com")
 
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
